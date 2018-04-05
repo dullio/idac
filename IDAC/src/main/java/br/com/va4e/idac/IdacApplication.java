c0efd5d960950c2e2abba2f781cd346a366c2394
dullio <dullio@L502X>
2018-04-05 08:32:23 -0300

    Testes de configuração Spring MVC + JPA + Hibernate

--- IDAC/src/main/java/br/com/va4e/idac/IdacApplication.java
+++ IDAC/src/main/java/br/com/va4e/idac/IdacApplication.java
@@ -2,8 +2,12 @@ package br.com.va4e.idac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+//@ConfigurationProperties(prefix="app.datasource")//Get external properties
+//@Import(value=org.springframework.boot.context.properties.EnableConfigurationPropertiesImportSelector.class) //import a configuration class
 public class IdacApplication {
 
 	public static void main(String[] args) {
---
+++ IDAC/src/main/java/br/com/va4e/idac/config/IdacWebConfig.java
@@ -0,0 +1,15 @@
+package br.com.va4e.idac.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dullio
+ *
+ *
+ *Classe para configurações do Spring Boot
+ *Novas configurações e sobrescrever as automáticas que forem necessárias
+ */
+@Configuration
+public class IdacWebConfig {
+
+}
