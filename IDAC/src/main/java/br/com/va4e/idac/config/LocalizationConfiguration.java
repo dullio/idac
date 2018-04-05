c189cbac943c68778a92e218bddde2ff688056e8
dullio <dullio@L502X>
2018-04-05 14:02:26 -0300

    Correções localização i18n

--- IDAC/src/main/java/br/com/va4e/idac/config/LocalizationConfiguration.java
+++ IDAC/src/main/java/br/com/va4e/idac/config/LocalizationConfiguration.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -22,6 +23,16 @@ public class LocalizationConfiguration extends WebMvcConfigurerAdapter {
     }
 
     @Bean
+    ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource bundleMessageSource = new ReloadableResourceBundleMessageSource();
+        bundleMessageSource.setBasename("classpath:i18n/messages");
+        bundleMessageSource.setCacheSeconds(1800);
+        bundleMessageSource.setDefaultEncoding("UTF-8");
+        return bundleMessageSource;
+    }
+  
+    
+    @Bean
     LocaleChangeInterceptor localeChangeInterceptor(){
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
@@ -33,3 +44,6 @@ public class LocalizationConfiguration extends WebMvcConfigurerAdapter {
         interceptorRegistry.addInterceptor(localeChangeInterceptor());
     }
 }
+
+
+
