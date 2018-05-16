a6d4b8beac13702ef24c61c804cdecae49b35c82
dullio <dullio@va4e-dell>
2018-05-16 18:55:31 -0300

    heaer and footer fragments

--- IDAC/src/main/java/br/com/va4e/idac/controller/MemberController.java
+++ IDAC/src/main/java/br/com/va4e/idac/controller/MemberController.java
@@ -1,5 +1,17 @@
 package br.com.va4e.idac.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
 public class MemberController {
+	
+	// @RequestMapping({"/", "/index"})
+	 @RequestMapping("/list")
+	public String getIndex(){
+		
+		return "member-list";
+	}
 
 }
